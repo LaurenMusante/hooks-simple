a1e53bfc21da8b8ad2a6b81df6b0942542c36b7b
Lauren <Lauren.Musante@gmail.com>
2020-01-28 15:28:22 -0800

    Add logic for componentDidUpdate to make new API call when selected resource changes

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -4,14 +4,25 @@ import axios from 'axios';
 class ResourceList extends React.Component {
 	state = { resources: []};
 	async componentDidMount() {
+		//async is added to componentDidMount because we have to "await" the response. 
 		const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
 		this.setState({ resources: response.data})
+		// "data" is axios-specific. In our response object, axios nests all our data in the "data" property. 
 	}
-	// "data" is axios-specific. In our response object, axios nests all our data in the "data" property. 
 
-	//componentDidMount because we want to make the call as soon as our component loads. The URL is from the documentation on website. Using string interpolation to tack on the endpoint we want, based on user input. 
+	//we use prevProps to see if the currently selected resource has changed. ONLY if it changed, we want to make another request. 
+	async componentDidUpdate(prevProps) {
+		if (prevProps.resource !== this.props.resource){
+		const response = await axios.get(
+  	`https://jsonplaceholder.typicode.com/${this.props.resource}`
+		);
+		this.setState({ resources: response.data });
+		}
+	}
 
-	//async is added to componentDidMount because we have to "await" the response. 
+	//componentDidMount because we want to make the call as soon as our component loads. The URL is from the documentation on website. Using string interpolation to tack on the endpoint we want, based on user input. This method ONLY gets invoked a single time, so it only makes a single API call. 
+	//so we use componentDidUpdate - gets called every time our component rerenders. 
+
 	render() {
 		return (
 			<div>
