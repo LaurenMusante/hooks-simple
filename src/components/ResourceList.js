c37c1c3d40cf8292eea5083155125e2adbb09f41
Lauren <Lauren.Musante@gmail.com>
2020-01-28 17:03:46 -0800

    Refactor ResourceList into a functional compnent. Add  useEffect to replace  original lifecycle methods

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,36 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-	state = { resources: []};
-	async componentDidMount() {
-		//async is added to componentDidMount because we have to "await" the response. 
-		const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-		this.setState({ resources: response.data})
-		// "data" is axios-specific. In our response object, axios nests all our data in the "data" property. 
-	}
-
-	//we use prevProps to see if the currently selected resource has changed. ONLY if it changed, we want to make another request. 
-	async componentDidUpdate(prevProps) {
-		if (prevProps.resource !== this.props.resource){
-		const response = await axios.get(
-  	`https://jsonplaceholder.typicode.com/${this.props.resource}`
+const ResourceList = ({resource}) => {
+	const [resources, setResources] = useState([]);
+	//initializes our state to be an empty array and gives us access to that initial state with 'resources' and we can change it using 'set
+	const fetchResource = async (resource) => {
+		//note: "resource" is the prop: will be either 'posts' or 'todos'
+		const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`
 		);
-		this.setState({ resources: response.data });
-		}
+
+		setResources(response.data);
+		// use to be: this.setState({ resources: response.data});
 	}
+//this will run every time our component mounts or updates
+	useEffect(() => {
+		fetchResource(resource); //resource is the prop
+	}, [resource])
+	//[resource] is the second argument that we are passing into useEffect. When it changes, useEffect is called. If I click on "posts" over and over again, it doesn't change the [resource] so it won't call useEffect/make another API call again. SO this replaces the "if prevProps !== props, then make API call".
 
-	//componentDidMount because we want to make the call as soon as our component loads. The URL is from the documentation on website. Using string interpolation to tack on the endpoint we want, based on user input. This method ONLY gets invoked a single time, so it only makes a single API call. 
-	//so we use componentDidUpdate - gets called every time our component rerenders. 
-
-	render() {
 		return (
 			<div>
-				{this.props.resource}
-				{/* this is what we will use to make a call to API */}
+				{/* only works w/in a class component: {this.props.resource} becomes... */}
+				{resources.length}
+		
 			</div>
 		)
-	}
+	
 }
 
 export default ResourceList;
